fac1b04a0659c9bbd790fd7f0b11abe3dd58fcf8
Jordan Williams <129204588+JordansFamiliar@users.noreply.github.com>
2024-02-12 16:53:52 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,11 @@
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
     matrix = []
 
     if n <= 0:
