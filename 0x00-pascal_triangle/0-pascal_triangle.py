dd8d2a428c32f36feda1c64f778fadc0ac3553e4
Jordan <measlybean@gmail.com>
2024-02-12 14:51:01 +0000

    Added files

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+"""A function that returns a list of lists of
+   integers representing Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    matrix = []
+
+    if n <= 0:
+        return matrix
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = matrix[i - 1][j - 1] + matrix[i - 1][j]
+                row.append(value)
+        matrix.append(row)
+
+    return matrix
